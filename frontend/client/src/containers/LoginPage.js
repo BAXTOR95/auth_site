00901f215d1f20da65e7230e438a4b4b7bb67da1
Brian Arriaga <brian.arriaga@gmail.com>
2022-08-12 20:18:46 -0400

    Added registration on frontend, route handler on express, async action creator and setup proxy

--- frontend/client/src/containers/LoginPage.js
+++ frontend/client/src/containers/LoginPage.js
@@ -1,11 +1,20 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { resetRegistered } from 'features/user';
 import Layout from 'components/Layout';
 
 const LoginPage = () => {
-    return (
-        <Layout title='Auth Site | Login' content='Login page'>
-            <h1>Login</h1>
-        </Layout>
-    );
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(resetRegistered());
+	}, [dispatch]);
+
+	return (
+		<Layout title='Auth Site | Login' content='Login page'>
+			<h1>Login</h1>
+		</Layout>
+	);
 };
 
 export default LoginPage;
